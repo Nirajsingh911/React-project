b12649691be0b371e10974d3843d4ef12d2e9bcf
Nirajsingh911 <101064463+Nirajsingh911@users.noreply.github.com>
2022-06-25 20:53:01 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,106 @@
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="A bg-dark" >
+      <h1 className=' heading me-2 '> Hacking Services And  Hacker</h1>
+      <hr />
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-3 p-3'>
+            <div class="card" >
+              <img src="img1.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <h5 class="card-title">NirajSingh</h5>
+                <p class="card-text">Hacking Services=>Bug-bonding,Social-Media Hacking,Website hacking,Smartphon,PC Hacking.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+
+            </div>
+          </div>
+          <div className='col-md-3 p-3'>
+            <div class="card" >
+              <img src="img2.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+
+            </div>
+          </div>
+          <div className='col-md-3 p-3'>
+            <div class="card" >
+              <img src="img7.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+
+            </div>
+          </div>
+          <div className='col-md-3 p-3'>
+            <div class="card" >
+              <img src="img8.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+
+            </div>
+          </div>
+          <div className='col-md-3 p-3'>
+            <div class="card" >
+              <img src="img9.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+
+            </div>
+          </div>
+          <div className='col-md-3 p-3'>
+            <div class="card" >
+              <img src="img6.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+
+            </div>
+          </div><div className='col-md-3 p-3'>
+            <div class="card" >
+              <img src="img11.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <h5 class="card-title">Shoeb Khan</h5>
+                <p class="card-text">Most Wanted Cyber Criminal. Shobe Khan has Hack U.S Bank steal Pivaricy of Smartphone User,Crime -> Ladkiyo ko chad na = Sorry!!Majak kar raha hu .</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+
+            </div>
+          </div>
+          <div className='col-md-3 p-3'>
+            <div class="card" >
+              <img src="img12.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <h5 class="card-title">Mohit Shahu</h5>
+                <p class="card-text">Mohit Shahu World Wanted Cyber Criminal. Crime-> Website Hacking,Bank Hacking,Account Hacking,Aur Niraj Ka Phone nahi uttana!! Thujhe Sorry nahi Boluga</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
